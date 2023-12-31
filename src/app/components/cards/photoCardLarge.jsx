e3e5b4568b0627b73319bac233ad8ddbf6e321a0
YuriyMitrofanov <yurasell@yandex.ru>
2023-12-31 14:41:26 +0300

    handleDelete method has been moved from photoCardLarge up to photoPage

--- src/app/components/cards/photoCardLarge.jsx
+++ src/app/components/cards/photoCardLarge.jsx
@@ -1,50 +1,54 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+// import { useNavigate, useParams } from "react-router-dom";
 import DeleteButton from "../buttons/deleteButton";
 import PropTypes from "prop-types";
-import { getCurrentGallery, updateGallery } from "../../store/galleries";
+// import { getCurrentGallery, updateGallery } from "../../store/galleries";
 import { getUserAccountType } from "../../store/users";
 
-const PhotoCardLarge = ({ image, id }) => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { galleryId } = useParams();
-    const currentGallery = useSelector(getCurrentGallery(galleryId));
-    const { photos, titlePhoto } = currentGallery;
+const PhotoCardLarge = ({ img, photoId, onDelete }) => {
+    // const dispatch = useDispatch();
+    // const navigate = useNavigate();
+    // const { galleryId } = useParams();
+    // const currentGallery = useSelector(getCurrentGallery(galleryId));
+    // const { photos, titlePhoto } = currentGallery;
     const isAdmin = useSelector(getUserAccountType());
-
-    const handleDelete = () => {
-        const editedPhotos = photos.filter(item => item !== id);
-        const editedTitlePhoto = titlePhoto === id ? editedPhotos[0] : titlePhoto;
-        const editedGallery = {
-            ...currentGallery,
-            photos: [...editedPhotos],
-            titlePhoto: editedTitlePhoto
-        };
-        try {
-            dispatch(updateGallery(editedGallery));
-        } catch (error) {
-            console.error(error.message);
-        } finally {
-            navigate(-1);
-        }
+    const handleDelete = (photoId) => {
+        onDelete(photoId);
     };
+
+    // const handleDelete = () => {
+    //     const editedPhotos = photos.filter(item => item !== id);
+    //     const editedTitlePhoto = titlePhoto === id ? editedPhotos[0] : titlePhoto;
+    //     const editedGallery = {
+    //         ...currentGallery,
+    //         photos: [...editedPhotos],
+    //         titlePhoto: editedTitlePhoto
+    //     };
+    //     try {
+    //         dispatch(updateGallery(editedGallery));
+    //     } catch (error) {
+    //         console.error(error.message);
+    //     } finally {
+    //         navigate(-1);
+    //     }
+    // };
     return (
         <div className="photo-card">
             <img
                 className="photo-card-img"
-                src={image}
+                src={img}
                 alt="photo"
             />
-            {isAdmin === "admin" && <DeleteButton onDelete={handleDelete}/>}
+            {isAdmin === "admin" && <DeleteButton onDelete={() => handleDelete(photoId)}/>}
         </div>
     );
 };
 
 PhotoCardLarge.propTypes = {
-    image: PropTypes.string,
-    id: PropTypes.string
+    img: PropTypes.string,
+    photoId: PropTypes.string,
+    onDelete: PropTypes.func
 };
 
 export default PhotoCardLarge;
--- src/app/pages/photoPage.jsx
+++ src/app/pages/photoPage.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
 import { getCurrentPhoto } from "../store/photos";
 import BackButton from "../components/buttons/backButton";
 import PhotoCardLarge from "../components/cards/photoCardLarge";
+import { getCurrentGallery, updateGallery } from "../store/galleries";
 
 const PhotoPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { photoId } = useParams();
     const currentPhoto = useSelector(getCurrentPhoto(photoId));
+    const { galleryId } = useParams();
+    const currentGallery = useSelector(getCurrentGallery(galleryId));
+    const { photos, titlePhoto } = currentGallery;
+
+    const handleDelete = ({ id }) => {
+        const editedPhotos = photos.filter(item => item !== id);
+        const editedTitlePhoto = titlePhoto === id ? editedPhotos[0] : titlePhoto;
+        const editedGallery = {
+            ...currentGallery,
+            photos: [...editedPhotos],
+            titlePhoto: editedTitlePhoto
+        };
+        dispatch(updateGallery(editedGallery));
+        navigate(-1);
+    };
+
     return (
         <div className="photo-container">
-            <PhotoCardLarge image={currentPhoto.URL} id={photoId}/>
+            <PhotoCardLarge
+                img={currentPhoto.URL}
+                photoId={photoId}
+                onDelete={handleDelete}
+            />
             <BackButton/>
         </div>
     );
